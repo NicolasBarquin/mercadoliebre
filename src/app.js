a0c3a822490d25b2d7bbc869840a7f14146d0124
¨NicolasBarquin¨ <nicolasbarquin1@gmail.com>
2023-11-07 10:45:45 -0300

    first commit

--- src/app.js
+++ src/app.js
@@ -9,6 +9,15 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "views", "home.html"))
 })
 
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "register.html"))
+})
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "login.html"))
+})
+
+
 app.listen(PORT, () => {
     console.log("El servidor esta corriendo en el puerto " + PORT);
 })
